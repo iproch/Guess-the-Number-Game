7f31cbece71f74fdfc5f7904573ef82a715c3e0d
Isaac Proch <isaac@iproch.design>
2019-12-03 22:02:28 +0000

    Adds guessCount to display user their total attempts.

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
     Random random = new Random();
     int min = 0;
     int max = 100;
+    int guessCount = 0;
     int winningNum = random.nextInt(((max - min) + 1) - min);
     Scanner userInput = new Scanner(System.in);
     boolean winner = false;
@@ -14,15 +15,17 @@ class Main {
     
     if(userGuess != winningNum){
       if(userGuess < winningNum){
+        guessCount++;
         System.out.println("Try guessing higher!");
       }
       else{
+        guessCount++;
         System.out.println("Try guessing lower!");
       }
     }
     else{
       winner = true;
-      System.out.println("You guessed correctly! The number was " + winningNum);
+      System.out.println("You guessed correctly after " + guessCount + " attempts!" + "The number was " + winningNum);
     }
     }
   }
