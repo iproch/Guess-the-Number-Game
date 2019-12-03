286442437c0af6364f79250d95ba87f0e5ffa9d8
Isaac Proch <isaac@iproch.design>
2019-12-03 21:47:43 +0000

    Syncs VS Code Version. Adds User input with Scanner. Adds winner boolean. Adds while loop that corresponds with winner boolean.

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
-import java.util.Random;
+import java.util.*;
 class Main {
-  public static void main(String[] args) {
-    Random random = new Random(100);
+  public static void main(final String[] args) {
+    Random random = new Random();
     int min = 0;
     int max = 100;
-    int winningNum = random.nextInt(((max-min)+1)-min);
+    int winningNum = random.nextInt(((max - min) + 1) - min);
+    Scanner userInput = new Scanner(System.in);
+    boolean winner = false;
+
+    while(!winner){
+    System.out.println("Enter your guess.");
     System.out.println(winningNum);
+    winner = true;
+    }
   }
 }
